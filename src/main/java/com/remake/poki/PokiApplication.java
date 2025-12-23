129ce9182e2d2b000cd3883a86ce276490f2b719
LAPTOP-QO0M5VLN\ASUS <vuhongthien>
2025-12-23 23:14:24 +0700

    update admin

--- src/main/java/com/remake/poki/PokiApplication.java
+++ src/main/java/com/remake/poki/PokiApplication.java
@@ -1,13 +1,20 @@
 package com.remake.poki;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class PokiApplication {
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokiApplication.class, args);
